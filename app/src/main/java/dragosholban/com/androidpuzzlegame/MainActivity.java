abf38502481a6954893fb86dd5ad4e0aa3088ebc
Dragos Holban <dragos@intelligentbee.com>
2018-03-08 16:18:35 +0200

    Cut the Puzzle Pieces

--- app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.java
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/MainActivity.java
@@ -1,7 +1,12 @@
 package dragosholban.com.androidpuzzlegame;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
@@ -73,13 +78,99 @@ public class MainActivity extends AppCompatActivity {
         for (int row = 0; row < rows; row++) {
             int xCoord = 0;
             for (int col = 0; col < cols; col++) {
-                Bitmap pieceBitmap = Bitmap.createBitmap(croppedBitmap, xCoord, yCoord, pieceWidth, pieceHeight);
+                // calculate offset for each piece
+                int offsetX = 0;
+                int offsetY = 0;
+                if (col > 0) {
+                    offsetX = pieceWidth / 3;
+                }
+                if (row > 0) {
+                    offsetY = pieceHeight / 3;
+                }
+
+                // apply the offset to each piece
+                Bitmap pieceBitmap = Bitmap.createBitmap(croppedBitmap, xCoord - offsetX, yCoord - offsetY, pieceWidth + offsetX, pieceHeight + offsetY);
                 PuzzlePiece piece = new PuzzlePiece(getApplicationContext());
                 piece.setImageBitmap(pieceBitmap);
-                piece.xCoord = xCoord + imageView.getLeft();
-                piece.yCoord = yCoord + imageView.getTop();
-                piece.pieceWidth = pieceWidth;
-                piece.pieceHeight = pieceHeight;
+                piece.xCoord = xCoord - offsetX + imageView.getLeft();
+                piece.yCoord = yCoord - offsetY + imageView.getTop();
+                piece.pieceWidth = pieceWidth + offsetX;
+                piece.pieceHeight = pieceHeight + offsetY;
+
+                // this bitmap will hold our final puzzle piece image
+                Bitmap puzzlePiece = Bitmap.createBitmap(pieceWidth + offsetX, pieceHeight + offsetY, Bitmap.Config.ARGB_8888);
+
+                // draw path
+                int bumpSize = pieceHeight / 4;
+                Canvas canvas = new Canvas(puzzlePiece);
+                Path path = new Path();
+                path.moveTo(offsetX, offsetY);
+                if (row == 0) {
+                    // top side piece
+                    path.lineTo(pieceBitmap.getWidth(), offsetY);
+                } else {
+                    // top bump
+                    path.lineTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 3, offsetY);
+                    path.cubicTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 6, offsetY - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 6 * 5, offsetY - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 3 * 2, offsetY);
+                    path.lineTo(pieceBitmap.getWidth(), offsetY);
+                }
+
+                if (col == cols - 1) {
+                    // right side piece
+                    path.lineTo(pieceBitmap.getWidth(), pieceBitmap.getHeight());
+                } else {
+                    // right bump
+                    path.lineTo(pieceBitmap.getWidth(), offsetY + (pieceBitmap.getHeight() - offsetY) / 3);
+                    path.cubicTo(pieceBitmap.getWidth() - bumpSize,offsetY + (pieceBitmap.getHeight() - offsetY) / 6, pieceBitmap.getWidth() - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6 * 5, pieceBitmap.getWidth(), offsetY + (pieceBitmap.getHeight() - offsetY) / 3 * 2);
+                    path.lineTo(pieceBitmap.getWidth(), pieceBitmap.getHeight());
+                }
+
+                if (row == rows - 1) {
+                    // bottom side piece
+                    path.lineTo(offsetX, pieceBitmap.getHeight());
+                } else {
+                    // bottom bump
+                    path.lineTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 3 * 2, pieceBitmap.getHeight());
+                    path.cubicTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 6 * 5,pieceBitmap.getHeight() - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 6, pieceBitmap.getHeight() - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 3, pieceBitmap.getHeight());
+                    path.lineTo(offsetX, pieceBitmap.getHeight());
+                }
+
+                if (col == 0) {
+                    // left side piece
+                    path.close();
+                } else {
+                    // left bump
+                    path.lineTo(offsetX, offsetY + (pieceBitmap.getHeight() - offsetY) / 3 * 2);
+                    path.cubicTo(offsetX - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6 * 5, offsetX - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6, offsetX, offsetY + (pieceBitmap.getHeight() - offsetY) / 3);
+                    path.close();
+                }
+
+                // mask the piece
+                Paint paint = new Paint();
+                paint.setColor(0XFF000000);
+                paint.setStyle(Paint.Style.FILL);
+
+                canvas.drawPath(path, paint);
+                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+                canvas.drawBitmap(pieceBitmap, 0, 0, paint);
+
+                // draw a white border
+                Paint border = new Paint();
+                border.setColor(0X80FFFFFF);
+                border.setStyle(Paint.Style.STROKE);
+                border.setStrokeWidth(8.0f);
+                canvas.drawPath(path, border);
+
+                // draw a black border
+                border = new Paint();
+                border.setColor(0X80000000);
+                border.setStyle(Paint.Style.STROKE);
+                border.setStrokeWidth(3.0f);
+                canvas.drawPath(path, border);
+
+                // set the resulting bitmap to the piece
+                piece.setImageBitmap(puzzlePiece);
+
                 pieces.add(piece);
                 xCoord += pieceWidth;
             }
